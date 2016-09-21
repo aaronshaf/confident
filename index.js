6b5f44b6c692ca855e5163066ff656c55988d852
Aaron Shafovaloff <aaronshaf@gmail.com>
2016-09-21 13:48:13 -0600

    show static api.yml file

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router()
-const yaml = require('js-yaml')
+const safeLoad = require('js-yaml').safeLoad
 const fs = require('fs')
+const path = require('path')
 
 module.exports = function (options) {
-  const definition = yaml.safeLoad(fs.readFileSync(options.definition, 'utf8'))
+  const yaml = fs.readFileSync(options.definition, 'utf8')
+  const definition = safeLoad(yaml)
+  router.get('/' + path.basename(options.definition), (req, res) => {
+    res.header('Content-Type', 'text/yaml').send(yaml)
+  })
   Object.keys(definition.paths).forEach((path) => {
     Object.keys(definition.paths[path]).forEach((method) => {
       router[method](path, options.operations[definition.paths[path][method].operationId])
